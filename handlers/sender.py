8e468008534ee604012fd2687aa621a926bc6963
YourStyle <chukanil@gmail.com>
2024-09-02 14:19:14 +0300

    fix

--- handlers/sender.py
+++ handlers/sender.py
@@ -1,5 +1,6 @@
 from aiogram import Router, Bot
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from database import database
 import random
@@ -10,7 +11,8 @@ ADMIN_ID = 416546809
 
 
 @winner_router.message(Command("sendresults"))
-async def command_best_user(message: Message, bot: Bot) -> None:
+async def command_best_user(message: Message, bot: Bot, state: FSMContext) -> None:
+    await state.clear()
     if message.from_user.id != ADMIN_ID:
         await message.answer("У вас нет прав для выполнения этой команды.")
         return
@@ -20,10 +22,20 @@ async def command_best_user(message: Message, bot: Bot) -> None:
         for place in users:
             cur_place = f"{cnt}-{cnt + len(place['results'])}"
             for u in place["results"]:
-                await bot.send_message(u, f"Ваше место в списке лидеров:{cur_place}")
+                try:
+                    await bot.send_message(u, f"Ваше место в списке лидеров:{cur_place}")
+                except Exception as e:
+                    print(f"Ошибка при отправке сообщения пользователю {u}: {e}")
+                    pass
+
             cnt += 1
         shuffled = users[0]["results"]
         random.shuffle(shuffled)
         for i in shuffled[0:10]:
-            await bot.send_message(i, f"Поздравляем! Вы победитель")
-            database.pick_winners(i)
+            try:
+                await bot.send_message(i, f"Поздравляем! Вы победитель")
+                database.pick_winners(i)
+            except Exception as e:
+                print(f"Ошибка при отправке сообщения пользователю {i}: {e}")
+                pass
+
